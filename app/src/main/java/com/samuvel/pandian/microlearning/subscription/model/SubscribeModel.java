de6b72c87d3dcd4c32a8eb9ee099e6fcffc406c3
Samuvel P <samuvelpandian@gmail.com>
2019-02-15 21:38:16 +0530

    service method for getting learning links

--- app/src/main/java/com/samuvel/pandian/microlearning/subscription/model/SubscribeModel.java
+++ app/src/main/java/com/samuvel/pandian/microlearning/subscription/model/SubscribeModel.java
@@ -2,11 +2,9 @@ package com.samuvel.pandian.microlearning.subscription.model;
 
 import android.util.Log;
 
-import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.TaskCompletionSource;
 import com.samuvel.pandian.microlearning.subscription.presenter.SubscribePresenterContract;
 
-import java.io.IOException;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -14,7 +12,6 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public class SubscribeModel implements SubscribeModelContract {
@@ -46,11 +43,36 @@ public class SubscribeModel implements SubscribeModelContract {
         call.enqueue(new Callback<String>() {
             @Override
             public void onResponse(Call<String> call, Response<String> response) {
-
+                if (getPresenter() != null) {
+                    getPresenter().onSubscribeSuccess();
+                }
             }
 
             @Override
             public void onFailure(Call<String> call, Throwable t) {
+                if (getPresenter() != null) {
+                    getPresenter().onSubscribeFailed();
+                }
+            }
+        });
+    }
+
+    private void getUserLearningLinks(String userId) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        MicrolearningService service = retrofit
+                .create(MicrolearningService.class);
+        Call<List<String>> call = service.getLearningLinks(userId);
+        call.enqueue(new Callback<List<String>>() {
+            @Override
+            public void onResponse(Call<List<String>> call, Response<List<String>> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<List<String>> call, Throwable t) {
 
             }
         });
@@ -65,5 +87,8 @@ public class SubscribeModel implements SubscribeModelContract {
         Call<String> subscribeToTopic(@Path("uuid") String uuid,
                                       @Path("topic") String topic,
                                       @Path("devicetoken") String devicetoken);
+
+        @GET("learninglinks/{uuid}")
+        Call<List<String>> getLearningLinks(@Path("uuid") String uuid);
     }
 }
